e19434d225bdee8df6c4428fbf6cabfd7d67a4df
yanhao <yhzhu@umich.edu>
2019-08-18 13:56:14 -0400

    Add files

---
+++ multi_link/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yanhao/Desktop/ROS/ROS_moveit/multi_link/src
+# Build directory: /home/yanhao/Desktop/ROS/ROS_moveit/multi_link/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("visualize_robot")
---
+++ multi_link/build/visualize_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yanhao/Desktop/ROS/ROS_moveit/multi_link/src/visualize_robot
+# Build directory: /home/yanhao/Desktop/ROS/ROS_moveit/multi_link/build/visualize_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
